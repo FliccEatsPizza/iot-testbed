f6abf04f36490b48b6d0ff6f759504a256996850
FliccEatsPizza <2023csb1149@iitrpr.ac.in>
2026-08-24 22:21:05 +0530

    Update default sandbox image to gcc:latest

--- gateway_client/sandbox_runner.py
+++ gateway_client/sandbox_runner.py
@@ -6,7 +6,7 @@ from typing import List, Optional
 
 logger = logging.getLogger("sandbox_runner")
 
-SANDBOX_IMAGE = os.getenv("SANDBOX_IMAGE", "gcc:12-slim")
+SANDBOX_IMAGE = os.getenv("SANDBOX_IMAGE", "gcc:latest")
 DOCKER_NETWORK = os.getenv("SANDBOX_NETWORK", "iot-testbed-sandbox")
 DOWNLOAD_DIR = os.getenv("DOWNLOAD_DIR", "./downloads")
 
